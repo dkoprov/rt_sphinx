6815f0e61379f972336938df2c18e2cb8b29754c
Dmitry Koprov <dmitry.koprov@evrone.ru>
2013-07-24 13:07:37 +0400

    no need for index so far

--- lib/rt_sphinx/query.rb
+++ lib/rt_sphinx/query.rb
@@ -7,15 +7,15 @@ module RtSphinx
       @connection = RtSphinx::Connection.instance
     end
 
-    def select(index, clause)
-      select_sql = "SELECT * FROM #{index} WHERE #{clause}"
+    def select(clause)
+      select_sql = "SELECT * FROM rt_logs WHERE #{clause}"
       @connection.execute_sql(select_sql, 'select')
     end
 
-    def insert(index, *values)
+    def insert(*values)
       item_id = rand(10_000)*rand(9_888)
       values_str = values.map{|v| "'#{v}'"}.join(', ')
-      insert_sql = "INSERT INTO #{index} (id, timestamp, message, logjson) VALUES (#{item_id}, #{values_str})"
+      insert_sql = "INSERT INTO rt_logs (id, timestamp, message, logjson) VALUES (#{item_id}, #{values_str})"
       @connection.execute_sql(insert_sql, 'insert')
     end
   end
